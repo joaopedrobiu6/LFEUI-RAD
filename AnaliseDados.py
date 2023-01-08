25770d06733918fc478c2c2c3268d4e432fc4218
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2023-01-08 18:59:52 +0000

    Fit working

--- AnaliseDados.py
+++ AnaliseDados.py
@@ -14,18 +14,30 @@ def plot(x, y, color_, marker_, linestyle_, title_, xlabel_, ylabel_, filename_)
     return p1, x, y
 
 
-def Func_to_Fit(par1_, par2_, x):
-    y = np.exp(par2_*(x-par1_))
+def Func_to_Fit(x, A, B, C):
+    y = A*np.exp(B*(x-C))
     return y
 
-def Fit(x, y):
+def Fit(x, y, min, max, color1_, color2_, marker_, linestyle_, title_, xlabel_, ylabel_, filename_):
+    fig, ax = plt.subplots(1, 1)
+    
     parameters, covariance = curve_fit(Func_to_Fit, x, y, maxfev=2000)
-
     fit_A = parameters[0]
     fit_B = parameters[1]
-    print(fit_A, fit_B)
-    fit_y = Func_to_Fit(fit_A, fit_B, x)
-    #plot(x, fit_y, "r", "o", "-", "titulo", "xaxis", "yaxis", "savefigura.png")
+    fit_C = parameters[2]
+    print(f"\npar[0] = {fit_A}\npar[1] = {fit_B}\npar[2] = {fit_C}\n")
+    fit_x = np.arange(min, max, 0.05 )
+    
+    ax.scatter(x, y, marker=marker_, color=color1_)
+    ax.plot(fit_x, Func_to_Fit(fit_x, fit_A, fit_B, fit_C), color=color2_, linestyle=linestyle_)
+    #ax.annotate([f"par[0] = {fit_A}\n, par[1] = {fit_B},\n fpar[2] = {fit_C}"], (1, 1))
+
+    ax.set_xlabel(xlabel_)
+    ax.set_ylabel(ylabel_)
+    ax.set_title(title_)
+    
+    fig.savefig(filename_)
+    
     return parameters
     
     
@@ -39,4 +51,9 @@ def Thresh_Data(i_cap):
     print('\nThreshold voltage (math method):    ' + str(tv2))
 
 
-Thresh_Data(0.001)
+xdata = [1, 2, 3, 4, 5]
+xdata = np.asarray(xdata)
+ydata = 2*np.exp(xdata)
+ydata = np.asarray(ydata)
+
+Fit(xdata, ydata, 0, 5.1, "red", "orange", "^", "--", "ola", "xx", "yy", "tentativa.png")
